1d67fe60ce925068d8c451726bc4540ad1326df1
Anton <jeanleonov@gmail.com>
2013-04-25 00:28:22 +0300

    Initial commit

---
+++ MathModel/FourHinger/src/model/FourHinger.java
@@ -0,0 +1,18 @@
+package model;
+
+public class FourHinger { 
+	
+	private double baseLength;
+	private static final double otherLegth = 1;
+
+	public FourHinger(double baseLength) {
+		this.baseLength = baseLength;
+	}
+	
+	/** previousTeta for avoiding jumps */
+	public double getTeta(double alpha, double previousTeta) {
+		// TODO
+		return 0;
+	}
+	
+}
